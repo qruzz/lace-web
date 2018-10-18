61f403d727d8902a483ed3f7353339dcf31c02c7
Michael Nissen <m9ssen@gmail.com>
2018-10-18 17:28:33 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      support: null,
+      mediaStream: null,
+    };
+
+    this.MEDIA_STREAM = null;
+  }
+
+  componentDidMount() {
+    const support = !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+
+    if (support) {
+      navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          width: 608,
+          height: 608,
+        },
+      }).then((result) => {
+        console.log(result);
+
+        this.setState({
+          support: true,
+          mediaStream: result,
+        });
+      });
+    } else {
+      this.setState({
+        support: 'getUserMedia() IS NOT supported by this browser',
+      });
+    }
+  }
+
+  renderCameraInput = () => {
+    if (this.state.support && this.state.mediaStream) {
+        this.VIDEO.srcObject = this.state.mediaStream;
+    }
+
+    return (
+      <h1>{this.state.support}</h1>
+    )
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.renderCameraInput()}
+        <video
+          ref={(ref) => this.VIDEO = ref}
+          controls
+          muted
+          playsInline
+          autoPlay
+          height="608"
+          width="608"
+          className="video-container"
+        />
       </div>
     );
   }
