cda74a91c80467962bcb777b1d080823858bed1a
Michael Nissen <m9ssen@gmail.com>
2018-10-25 17:21:49 +1100

    Added build folder

---
+++ build/precache-manifest.f443dc1b06c69e56b092dc7497430293.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e21847f770d495596f954080baccc3f",
+    "url": "/static/media/logo.5e21847f.svg"
+  },
+  {
+    "revision": "1f8e9a89f085dbe7ca9a19cf3ee83e7a",
+    "url": "/static/media/logo-white.1f8e9a89.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b42997a8c6b276f37ac",
+    "url": "/static/js/main.7b42997a.chunk.js"
+  },
+  {
+    "revision": "b3ebf6b483802db0cb01",
+    "url": "/static/js/1.b3ebf6b4.chunk.js"
+  },
+  {
+    "revision": "7b42997a8c6b276f37ac",
+    "url": "/static/css/main.11fa2c11.chunk.css"
+  },
+  {
+    "revision": "9025fa85d97fd9b314f4f717217f8cb0",
+    "url": "/index.html"
+  }
+];
