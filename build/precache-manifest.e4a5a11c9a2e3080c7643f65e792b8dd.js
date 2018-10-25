fc58dde71abad5cfd6c38f6de5948a15041d3d4a
Michael Nissen <m9ssen@gmail.com>
2018-10-25 17:56:12 +1100

    New build

---
+++ build/precache-manifest.e4a5a11c9a2e3080c7643f65e792b8dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e21847f770d495596f954080baccc3f",
+    "url": "/static/media/logo.5e21847f.svg"
+  },
+  {
+    "revision": "1f8e9a89f085dbe7ca9a19cf3ee83e7a",
+    "url": "/static/media/logo-white.1f8e9a89.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f2ed972554a7e9930dc0",
+    "url": "/static/js/main.f2ed9725.chunk.js"
+  },
+  {
+    "revision": "b3ebf6b483802db0cb01",
+    "url": "/static/js/1.b3ebf6b4.chunk.js"
+  },
+  {
+    "revision": "f2ed972554a7e9930dc0",
+    "url": "/static/css/main.11fa2c11.chunk.css"
+  },
+  {
+    "revision": "590c3e9482d43a5b4a596f50ee031824",
+    "url": "/index.html"
+  }
+];
