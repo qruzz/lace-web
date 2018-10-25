cfc2fc36e7b6c44ea7fd9256c78c4129624bbbe8
Michael Nissen <m9ssen@gmail.com>
2018-10-25 17:20:48 +1100

    Auth is working

--- src/modules/Auth/Auth.js
+++ src/modules/Auth/Auth.js
@@ -3,31 +3,73 @@ import CryptoJS from 'crypto-js';
 
 import { connectUser } from '../../api.js';
 
-import './Auth.css';
+import './Auth.css'; 
+
+const logo = require('../../resources/logo-white.svg');
 
 export default class Auth extends PureComponent {
     constructor(props) {
         super(props);
 
         this.state = {
-            username: null,
-            password: null,
+            username: undefined,
+            password: undefined,
         };
     }
 
-    handleLogin = () => {
-        connectUser({
-            username: this.state.username,
-            password: CryptoJS.SHA3(this.state.password),
-        }).then((result) => {
-            console.log(result);
-        });
+    renderLoginButton = () => {
+        const handleLogin = () => {
+            connectUser({
+                username: this.state.username,
+                password: CryptoJS.SHA3(this.state.password).toString(),
+            }).then((result) => {
+                if (result) {
+                    localStorage.setItem('@User', JSON.stringify(result));
+                    this.props.history.push('/player');
+                }
+            });
+        };
+
+        return (
+            <button
+                className="login-button"
+                onClick={handleLogin}
+            >
+                Connect
+            </button>
+        );
     }
 
     render() {
         return (
-            <div className="main-container">
-                
+            <div className="auth-container">
+                <img
+                    className="brand-logo"
+                    alt='lace'
+                    src={logo}
+                />
+                <input
+                    className="auth-input"
+                    placeholder="username"
+                    value={this.state.username}
+                    onChange={(event) => {
+                        this.setState({
+                            username: event.target.value,
+                        });
+                    }}
+                />
+                <input
+                    className="auth-input"
+                    placeholder="password"
+                    type="password"
+                    value={this.state.password}
+                    onChange={(event) => {
+                        this.setState({
+                            password: event.target.value,
+                        });
+                    }}
+                />
+                {this.renderLoginButton()}
             </div>
         );
     }
